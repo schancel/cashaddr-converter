0e4301ce3ca0973415ac89d450c2b5a96560e7d2
Jason B. Cox <contact@jasonbcox.com>
2017-12-22 14:24:07 -0800

    Added tests to check legacy RIPEMD encoding and decoding

--- legacy/encode_test.go
+++ legacy/encode_test.go
@@ -1,12 +1,24 @@
 package legacy
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestToLegacyP2KH(t *testing.T) {
+func TestEncodeP2KH(t *testing.T) {
+	payload, _ := hex.DecodeString("010966776006953D5567439E5E39F86A0D273BEE")
+	legacy := &Address{
+		Version: P2KH,
+		Payload: payload,
+	}
+	address, err := legacy.Encode()
+	assert.Nil(t, err)
+	assert.Equal(t, "16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM", address, "Conversion failed")
+}
+
+func TestEncodeLegacyToCopayLegacyP2KH(t *testing.T) {
 	legacy, err := Decode("19NoN69ntmV9nKHBjArLJXXCNq3AvvMsqG")
 	assert.Nil(t, err)
 	legacy.Version = P2KHCopay
@@ -16,7 +28,7 @@ func TestToLegacyP2KH(t *testing.T) {
 
 }
 
-func TestToLegacyP2SH(t *testing.T) {
+func TestEncodeLegacyToCopayLegacyP2SH(t *testing.T) {
 	legacy, err := Decode("3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy")
 	assert.Nil(t, err)
 	legacy.Version = P2SHCopay
