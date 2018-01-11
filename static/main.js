7ef8d3b39a3e499ed2777cbaff9576900324408c
Alexei Broner <alexei.broner@gmail.com>
2018-01-10 18:50:35 -0600

    Frontend redo

--- static/main.js
+++ static/main.js
@@ -2,7 +2,6 @@
 
 // QR Codes: https://davidshimjs.github.io/qrcodejs/
 
-<<<<<<< HEAD
 ;(() => {
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -180,75 +179,3 @@ const scenes = {
 };
 
 })();
-=======
-function processResponse(result) {
-	const {cashaddr, legacy, copay} = result;
-
-	const html = `
-		<div class="pure-u-1">CashAddress: ${cashaddr}</div>
-		<div class="pure-u-1">Legacy: ${legacy}</div>
-		<div class="pure-u-1">Copay: ${copay}</div>
-		<div class="pure-u-1" style="height:2em"></div>
-		<div id="legacy" class="pure-u-1 pure-u-md-1-3 result">
-			<div class="result-label">Legacy</div>
-		</div>
-		<div id="cashaddr" class="pure-u-1 pure-u-md-1-3 result">
-			<div class="result-label">CashAddress</div>
-		</div>
-		<div id="copay" class="pure-u-1 pure-u-md-1-3 result">
-			<div class="result-label">Copay</div>
-		</div>`;
-
-	document.getElementById("result").innerHTML = html;
-
-	new QRCode(document.getElementById("cashaddr"), {
-		mode: 1,
-		text: result.cashaddr.toUpperCase(),
-		width: 256,
-		height: 256,
-		correctLevel: QRCode.CorrectLevel.L,
-	});
-
-	new QRCode(document.getElementById("legacy"), {
-		text: result.legacy,
-		width: 256,
-		height: 256,
-		correctLevel: QRCode.CorrectLevel.L,
-	});
-
-	new QRCode(document.getElementById("copay"), {
-		text: result.copay,
-		width: 256,
-		height: 256,
-		correctLevel: QRCode.CorrectLevel.L,
-	});
-}
-
-function processError(error) {
-	const html = `<div class="pure-u-1">Error: ${error}</div>`;
-	document.getElementById("result").innerHTML = html;
-}
-
-function handleSubmit(event) {
-	event.preventDefault();
-
-	var address = encodeURIComponent(document.getElementById("address").value);
-	const xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = () => {
-		if (xhr.readyState === 4) {
-			if (xhr.status === 200) {
-				processResponse(JSON.parse(xhr.responseText));
-			} else {
-				processError(xhr.responseText);
-			}
-		}
-	};
-	xhr.open('GET', `/convert?address=${address}`);
-	xhr.send();
-}
-
-window.addEventListener("load", function () {
-	var mainform = document.getElementById("mainform");
-	mainform.addEventListener("submit", handleSubmit);
-});
->>>>>>> JS refactor
